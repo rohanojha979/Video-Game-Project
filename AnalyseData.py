6d2c0f82200d2851a6b7df93608eafa910304708
rohanojha979 <rohanojha979@gmail.com>
2021-04-20 16:40:49 +0530

    ..

--- AnalyseData.py
+++ AnalyseData.py
@@ -7,7 +7,13 @@ class Analyse:
         self.cleanData()
 
     def cleanData(self):
-        self.df.drop(columns=[self.df.columns[0]], inplace=True)
+        self.df = df.drop(columns=['Rank'])
+        self.df.rename(columns={ 'NA_Sales': 'America', 'EU_Sales': 'Europe', 'JP_Sales': 'Japan', 'Other_Sales' : 'Other', 'Global_Sales' : 'Global' }, inplace = True)
+
+        self.df['Year'].fillna(0, inplace=True)
+        self.df['Year'] = df['Year'].astype('int')
+
+        self.df.set_index('Name', inplace=True)
 
     def getCategories(self):
         return self.df.groupby('Platform').count().sort_values('Global_Sales')['Global_Sales'][::-1]
