b09f5d70591a4d8696e71b02721c5930cfae0388
m-triple-m <triplem656@gmail.com>
2021-05-10 15:56:24 +0530

    ..

--- AnalyseData.py
+++ AnalyseData.py
@@ -1,11 +1,13 @@
 import pandas as pd
 
+
 class Analyse:
 
-    def __init__(self, path = 'datasets/vgsales.csv'):
+    def __init__(self, path='datasets/vgsales.csv'):
         self.df = pd.read_csv(path)
+        self.df = self.df[:1000]
         self.cleanData()
-        self.df_upd = self.df[self.df['year']!= 0]
+        self.df_upd = self.df[self.df['year'] != 0]
 
     def cleanData(self):
         self.df.drop(columns=[self.df.columns[0]], inplace=True)
@@ -13,32 +15,32 @@ class Analyse:
         self.df['year'] = self.df['year'].fillna(0)
         self.df['publisher'] = self.df['publisher'].fillna('unknown')
         self.df['year'] = self.df[self.df['year'].notna()]['year'].astype('int64')
-        self.df ['total_sales'] = (
-                          self.df['na_sales'] + 
-                          self.df['eu_sales'] + 
-                          self.df['jp_sales'] + 
-                          self.df['other_sales'])
+        self.df['total_sales'] = (
+            self.df['na_sales'] +
+            self.df['eu_sales'] +
+            self.df['jp_sales'] +
+            self.df['other_sales'])
 
     def getTopPublishersByCount(self, n):
         return self.df.groupby('publisher').count().sort_values('year', ascending=False)['year'].head(n)
 
     def getTopPublishersBySum(self, n):
-        return self.df.groupby('publisher').sum().sort_values('global_sales', ascending = False)['global_sales'].head(n)
+        return self.df.groupby('publisher').sum().sort_values('global_sales', ascending=False)['global_sales'].head(n)
 
     def getTopPublishersBySumInRegion(self, n, region):
-        return self.df.groupby('publisher').sum().sort_values(region, ascending = False)[region].head(n)
+        return self.df.groupby('publisher').sum().sort_values(region, ascending=False)[region].head(n)
 
     def getTopGenresByCount(self):
         return self.df.groupby('genre').count().sort_values('year', ascending=False)['year']
 
     def getTopGenresBySum(self):
-        return self.df.groupby('genre').sum().sort_values('global_sales', ascending = False)['global_sales']
+        return self.df.groupby('genre').sum().sort_values('global_sales', ascending=False)['global_sales']
 
     def getTopGenresByCountInRegion(self, region):
-        return self.df.groupby('genre').count().sort_values(region, ascending = False)[region]
+        return self.df.groupby('genre').count().sort_values(region, ascending=False)[region]
 
     def getTopGenresBySumInRegion(self, region):
-        return self.df.groupby('genre').sum().sort_values(region, ascending = False)[region]
+        return self.df.groupby('genre').sum().sort_values(region, ascending=False)[region]
 
     def getGenres(self):
         return self.df['genre'].unique()
--- app.py
+++ app.py
@@ -16,32 +16,20 @@ analysis = Analyse("datasets/vgsales.csv")
 
 st.title('Video Games Sales Analysis')
 st.image("https://blog.vinfotech.com/sites/default/files/styles/blog-list-img-new/public/how-to-build-a-successful-real-money-gaming-website-banner.png?itok=MJI4LKyG")
-st.markdown("""
-#### In my project, we would like to examine the most sold games in the global perspective, then examine the games in terms of genre and platform, the game genres according to the region with game names and finally the year the most sold games are released and their publishers. Also some information are given about games, publishers and platforms.
-""")
 
-st.markdown(" ")
-st.markdown(" ")
 st.markdown("----")
-
 
 sidebar = st.sidebar
 
-def viewForm():
-
-    st.plotly_chart(plot())
-
-    title = st.text_input("Report Title")
-    desc = st.text_area('Report Description')
-    btn = st.button("Submit")
-
-    if btn:
-        report1 = Report(title = title, desc = desc, data = "")
-        sess.add(report1)
-        sess.commit()
-        st.success('Report Saved')
 
 def viewDataset():
+    st.markdown("""
+    ### In this project, I have analyzed the most sold games in the global perspective, then analyze the games in terms of genre and platform, the game genres according to the region with game names and finally the year the most sold games are released and their publishers. Also some information are given about games, publishers and platforms.
+    """)
+
+    st.text(" ")
+    st.text(" ")
+    st.markdown("---")
     st.header("Dataset Used for Video Game Sales Analysis")
     dataframe = analysis.getDataframe()
 
@@ -60,7 +48,8 @@ def viewDataset():
         st.dataframe(dataframe.describe())
         st.markdown('---')
 
-        types = {'object' : 'Categorical', 'int64': 'Numerical', 'float64': 'Numerical'}
+        types = {'object': 'Categorical',
+                 'int64': 'Numerical', 'float64': 'Numerical'}
         types = list(map(lambda t: types[str(t)], dataframe.dtypes))
         st.header('Dataset Columns')
         for col, t in zip(dataframe.columns, types):
@@ -71,33 +60,30 @@ def viewDataset():
             cols[2].markdown('#### Type :')
             cols[3].markdown(f"## {t}")
 
-  
-
-
-def analyse():
-    data = analysis.getpublisher()
-    st.plotly_chart(plotBar(data.index, data.values))
-    
 
 def analysePlatform():
     data = analysis.getPlatform()
-    st.plotly_chart(plotBar(analysis.getPlatform(), 'VG Sales Platform Data', 'Name of Platform', 'Global sales in millions'))
+    st.plotly_chart(plotBar(analysis.getPlatform(
+    ), 'VG Sales Platform Data', 'Name of Platform', 'Global sales in millions'))
     st.markdown("""
     ### In this graph, it shows that the global sales in million dollars platform wise which the top platform is PS2 and revenue is about 1200 millions dollars and after that X360 and this platform revenue is about 980 million dollars , the PS2 platform is highly demanded which gamers like to play most game and this platform PCFX is not popular and revenue is about 0.03 million dollars """)
 
+
 def analysePublisher():
 
-    num = st.select_slider(options=[5, 10, 15, 20, 25, 30], label="Select the number of Publishers to show")
+    num = st.select_slider(
+        options=[5, 10, 15, 20, 25, 30], label="Select the number of Publishers to show")
     st.header('Top Publishers By Release Count')
-    st.plotly_chart(plotBar(analysis.getTopPublishersByCount(num), 'VG Sales Publisher Data', 'Name Of Publisher', 'Global sales in millions'))
+    st.plotly_chart(plotBar(analysis.getTopPublishersByCount(
+        num), 'VG Sales Publisher Data', 'Name Of Publisher', 'Global sales in millions'))
     st.markdown("""
     ### In this graph, it shows the top publishers which are the highest in global sales and the top publisher is electronics arts which total sales is about 1350 million dollars and Electronic Arts is a leading publisher of games on Console, PC and Mobile. We exist to inspire the world through Play. Electronic Arts is a leading publisher of games on Console, PC and Mobile. EA Play FIFA 21 Madden NFL 21 Apex Legends Star Wars.
-    # """) 
+    # """)
 
     st.markdown("""
     ### The second top publishers is activision which global sales is about 950 million dollars and Activision Publishing, Inc. is an American video game publisher based in Santa Monica, California. It currently serves as the publishing business for its parent company, Activision Blizzard, and consists of several subsidiary studios. Activision is one of the largest third-party video game publishers in the world and was the top United States publisher in 2016.
      """)
-    
+
     st.markdown("""
     """)
     st.markdown("""
@@ -112,65 +98,58 @@ def analysePublisher():
     ---------
     """)
     st.header('Top Publishers By Total Sales')
-    st.plotly_chart(plotBar(analysis.getTopPublishersBySum(num), 'Total Sales of Publisher Data', 'Name Of Publisher', 'Global sales in millions'))
+    st.plotly_chart(plotBar(analysis.getTopPublishersBySum(
+        num), 'Total Sales of Publisher Data', 'Name Of Publisher', 'Global sales in millions'))
 
     st.header('Top Publishers By Total Sales in Region')
     for region, name in analysis.getRegion():
-        st.plotly_chart(plotBar(analysis.getTopPublishersBySumInRegion(num, region), 'Total Sales in '+name, 'Top Publisher', 'Global sales in millions'))
+        st.plotly_chart(plotBar(analysis.getTopPublishersBySumInRegion(
+            num, region), 'Total Sales in '+name, 'Top Publisher', 'Global sales in millions'))
+
 
 def analyseGenre():
 
     st.header('Top Genres By Release Count')
-    st.plotly_chart(plotBar(analysis.getTopGenresByCount(), 'Top Genre', 'Name Of Genre', 'Global sales in millions'))
+    st.plotly_chart(plotBar(analysis.getTopGenresByCount(),
+                            'Top Genre', 'Name Of Genre', 'Global sales in millions'))
 
     st.header('Top Genres By Total Sales')
-    st.plotly_chart(plotBar(analysis.getTopGenresBySum(), ' Total Sales of Top Genre', 'Name Of Genre', 'Global sales in millions'))
+    st.plotly_chart(plotBar(analysis.getTopGenresBySum(
+    ), ' Total Sales of Top Genre', 'Name Of Genre', 'Global sales in millions'))
 
     st.header('Top Genres By Total Sales in Region')
     for region, name in analysis.getRegion():
-        st.plotly_chart(plotBar(analysis.getTopGenresBySumInRegion(region), 'Total Sales By Region of Top Genre '+name, 'Name Of Genre', 'Global sales in millions'))
+        st.plotly_chart(plotBar(analysis.getTopGenresBySumInRegion(
+            region), 'Total Sales By Region of Top Genre '+name, 'Name Of Genre', 'Global sales in millions'))
 
     st.header('Top Genres By Total Sales in Region')
     for region, name in analysis.getRegion():
-        st.plotly_chart(plotBar(analysis.getTopGenresByCountInRegion(region), 'Total Release Count in '+name, 'Name Of Genre', 'Global sales in millions'))
-   
+        st.plotly_chart(plotBar(analysis.getTopGenresByCountInRegion(
+            region), 'Total Release Count in '+name, 'Name Of Genre', 'Global sales in millions'))
+
 
 def analyseGameRelease():
     st.header("Year wise release")
-    st.dataframe(analysis.getYearWiseRelease())
-    st.plotly_chart(plotLine(analysis.getYearWiseRelease(), 'Game Growth By Year', 'Year', 'Global releases'))
+    st.plotly_chart(plotLine(analysis.getYearWiseRelease(),
+                             'Exponential Growth in Video Games sales can be seen after 2000 as many new Gaming Plaforms such as Playstation, Xbox etc. were released', 'Year', 'Global releases'))
     st.markdown("""
     ### In this graph , here the growth of video game year wise from 1980 up to 2020 and we see that in 2008 - 2009 the highest growth of game during this year because PC games are highly demanded after 2001and also gamers like to play but after 2009 we also see that, the graph goes deep because of the publisher not releases that type of game which gamers like it and also in 2015, the publisher releases good game and also by time gamers switches from PC to mobile.
     # """)
-    
-# def 
 
-def viewReport():
-    reports = sess.query(Report).all()
-    titlesList = [ report.title for report in reports ]
-    selReport = st.selectbox(options = titlesList, label="Select Report")
-    
-    reportToView = sess.query(Report).filter_by(title = selReport).first()
 
-    markdown = f"""
-        ## {reportToView.title}
-        ### {reportToView.desc}
-        
-    """
-
-    st.markdown(markdown)
-
+sidebar.title('Video Games Sales Analysis')
 sidebar.header('Choose Your Option')
-options = [ 'View Database', 'Timeline Analysis', 'Analyse Platform', 'Analyse Publisher', 'Analyse Genre' ]
-choice = sidebar.selectbox( options = options, label="Choose Action" )
-
-if choice == options[0]:
-    viewDataset()
-elif choice == options[1]:
-    analyseGameRelease()
-elif choice == options[2]:
-    analysePlatform()
-elif choice == options[3]:
-    analysePublisher()
-elif choice == options[4]:
-    analyseGenre()
+options = ['View Database', 'Timeline Analysis',
+           'Analyse Platform', 'Analyse Publisher', 'Analyse Genre']
+choice = sidebar.selectbox(options=options, label="Choose Action")
+with st.spinner("please wait.."):
+    if choice == options[0]:
+        viewDataset()
+    elif choice == options[1]:
+        analyseGameRelease()
+    elif choice == options[2]:
+        analysePlatform()
+    elif choice == options[3]:
+        analysePublisher()
+    elif choice == options[4]:
+        analyseGenre()
